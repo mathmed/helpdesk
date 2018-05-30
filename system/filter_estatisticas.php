3d79b236bcefe3934b42ef5128820d58c373e3e8
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2018-05-30 09:12:05 -0300

    filter problem correction

--- system/filter_estatisticas.php
+++ system/filter_estatisticas.php
@@ -1,15 +1,27 @@
 <?php
 	/* recuperando arquivos essencias do banco de dados */
+	session_start();
 	require "../db/config.php";
 	require "../db/connection.php";
 	require "../db/database.php";
 
 	/* recuperando informações enviadas via post */
+	
 	$dateIn = $_POST['dateIn'];
 	$dateFn = $_POST['dateFn'];
+	
 
-	/* retornando a url */
+	/* verifica se ja existe filtro de data */
+	
+	if(strpos($_SESSION['url_atual'], 'dateIn') > 0){
 
-	$location = "Location: ../src/estatisticas.php?dateIn=".$dateIn."&dateFn=".$dateFn;
+		$url = explode("&dateIn", $_SESSION['url_atual']);
+		$url = $url[0];
+	}else{
+		$url = $_SESSION['url_atual'];
+	}
+	
+	$url  = explode("src", $url);
+	$location = "Location: ../src".$url[1]."&dateIn=".$dateIn."&dateFn=".$dateFn;
 	header($location);
 ?>
