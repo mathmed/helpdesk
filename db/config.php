42b55678d61857c6a70e7184cfa91045db02a67b
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2018-07-09 09:16:17 -0300

    update

--- db/config.php
+++ db/config.php
@@ -5,7 +5,7 @@
 	define('DB_HOSTNAME', '');
 	define('DB_USERNAME', '');
 	define('DB_PASSWORD', '');
-	define('DB_DATABASE', '');
+	define('DB_DATABASE', 'chamados');
 	define('DB_PREFIX', 'cw');
 	define('DB_CHARSET', 'utf8');
 ?>
